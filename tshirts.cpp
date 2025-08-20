f5794256a16d691f13a7ab7a4279098517fdb11f
Amri07 <128670136+Amri07@users.noreply.github.com>
2025-08-20 14:28:39 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -3,11 +3,11 @@
 
 char size(int cms) {
     char sizeName = '\0';
-    if(cms <= 38) {
+    if(cms < 38) {
         sizeName = 'S';
     } else if(cms > 38 && cms < 42) {
         sizeName = 'M';
-    } else if(cms >= 42) {
+    } else if(cms > 42) {
         sizeName = 'L';
     }
     return sizeName;
@@ -18,8 +18,10 @@ void testTshirtSize() {
     assert(size(37) == 'S');
     assert(size(40) == 'M');
     assert(size(43) == 'L');
-    assert(size(42) == 'M'); //This will fail
+    assert(size(42) == 'L'); //New TestCase
+    assert(size(38) == 'S'); //New TestCase
     std::cout << "All is well (maybe!)\n";
 }
+
 
 
